f6aa4fc5fd9a452e8a11add267c8cfe36237e873
alpha-beta-soup <richard.m.law@gmail.com>
2016-06-25 17:26:44 +1200

    setup.py etc for distutils/pypi

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Wraps [geopy](https://github.com/geopy/geopy) geocoding to expose a simple way t
 0. Configure your desired/allowed geocoding providers (`config.yml`).
 1. Geocode an address using the errorgeopy `geocode` wrapper.
 2. Recieve a multipoint of the geocoded address locations, the [convex hull](http://scipy.github.io/devdocs/generated/scipy.spatial.ConvexHull.html) of the result locations, the [concave hull](http://blog.thehumangeo.com/2014/05/12/drawing-boundaries-in-python/) of the results, various types of centroid, and quantitative measures of spatial agreement across component providers---as well as maintaining access to the original results.
+
+<!-- TODO how to install and contribute fixes and improvements -->
--- setup.py
+++ setup.py
@@ -1,19 +1,59 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# python setup.py check
+# python setup.py sdist
+# python setup.py register sdist upload
 
+from distutils.core import setup
+
+# https://www.python.org/dev/peps/pep-0314/
+# https://pypi.python.org/pypi?:action=list_classifiers
 config = {
+    'name': 'errorgeopy',
+    'packages': ['errorgeopy'],
+    'version': "0.0.1",
     'description': 'Python geocoding in a disagreeing world',
     'author': 'Richard Law',
+    'author_email': 'richard.m.law@gmail.com',
     'url': 'https://github.com/alpha-beta-soup/errorgeopy',
     'download_url': 'https://github.com/alpha-beta-soup/errorgeopy/archive/master.zip',
-    'author_email': 'richard.m.law@gmail.com',
-    'version': '0.1',
-    'install_requires': ['nose','numpy','scipy','geopy'],
-    'packages': ['errorgeopy'],
+    'requires': ['nose','numpy','scipy','geopy','pyyaml','shapely','pyhull','sklearn'],
     'scripts': [],
-    'name': 'errorgeopy'
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: GIS"
+    ],
+    'long_description': """\
+Geocoding with automated indication of spatial error across providers
+---------------------------------------------------------------------
+
+Geocoding is the process of taking a string representing a real-world address,
+and resolving that to a single point location. However different providers will
+resolve the same addresses to different locations, and it can be important to
+consider this spatial error. For example, when determining the census tract an
+addresses should be linked to, there may be some error. This error can go
+unacknowledged but still be very influential on analysis, similar to the famous
+modifiable area-unit problem (MAUP), but for point representations of addresses.
+
+This library can be used to determine spatial error across selected geocoding
+providers. It can, for example, produce the convex hull of successful geocodes,
+so you can imagine an address as a polygon. These individual points are also
+available as a multipoint geometry.
+
+Intended to work with as many of the providers supported by geopy as you care to
+configure. Without configuration, will use free global provdiders that don't
+require API tokens.
+
+Only supports Python 3.
+"""
+
 }
 
 setup(**config)
